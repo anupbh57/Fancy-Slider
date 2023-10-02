cffa3fd9db1beec9d0f2b8db62c3469181f530e4
Anup Bhandari <s10064884@student.bhtafe.edu.au>
2023-10-03 10:11:58 +1100

    slider functions

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
-const slideWrapper = document.getElementById('slider');
-const slide = document.querySelectorAll('.slide');
-console.log(slide)
-slide[0].getAttribute('data-index') == 1 ? slide[0].classList.add('slide-active') : null;
-
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.getElementById("slider");
+    const prevBtn = document.getElementById("prev-btn");
+    const nextBtn = document.getElementById("next-btn");
+    const slideWidth = slider.querySelector(".slide").offsetWidth;
+    let currentIndex = 0;
+  
+    // Function to update the active card
+    function updateActiveCard() {
+      const activeIndex = Math.round(slider.scrollLeft / slideWidth);
+      currentIndex = activeIndex;
+      const slides = document.querySelectorAll(".slide");
+  
+      slides.forEach((slide) => {
+        slide.classList.remove("active");
+      });
+  
+      const activeSlide = document.querySelector(`.slide[data-index="${activeIndex + 1}"]`);
+      activeSlide.classList.add("active");
+    }
+  
+    // Event listener for the "Next" button
+    nextBtn.addEventListener("click", function () {
+      currentIndex = Math.min(currentIndex + 1, slider.children.length - 1);
+      slider.scrollTo({
+        left: currentIndex * slideWidth,
+        behavior: "smooth",
+      });
+      updateActiveCard();
+    });
+  
+    // Event listener for the "Previous" button
+    prevBtn.addEventListener("click", function () {
+      currentIndex = Math.max(currentIndex - 1, 0);
+      slider.scrollTo({
+        left: currentIndex * slideWidth,
+        behavior: "smooth",
+      });
+      updateActiveCard();
+    });
+  
+    // Initialize active card
+    updateActiveCard();
+  
+    // Event listener for scroll events to update the active card
+    slider.addEventListener("scroll", updateActiveCard);
+  });
+  
